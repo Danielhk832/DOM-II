a2e48b93d46c2698cf2fcafcec1c68b026c68a9a
Daniel Kish <daniel@Daniels-Mac-mini.local>
2021-09-01 03:28:19 +0700

    made some changes to push branch to canvas

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+// buttons
+const button1 = document.querySelector(".btn:nth-of-type(1)");
+const button2 = document.querySelector(".btn:nth-of-type(2)");
+const button3 = document.querySelector(".btn:nth-of-type(3)");
+
+button1.textContent = "1";
+button2.textContent = "2";
+button3.textContent = "3";
